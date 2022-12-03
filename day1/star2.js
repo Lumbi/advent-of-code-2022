dcc935e8279a97e7af9b406b36ba4d492d0d2378
Gabriel Lumbi <Lumbi@users.noreply.github.com>
2022-12-02 22:12:48 -0500

    Day 1

---
+++ day1/star2.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+
+const add = (a, b) => a + b
+const sum = (array) => array.reduce(add, 0)
+const max = (array) => array.sort()[array.length - 1]
+
+const input = fs.readFileSync('input.txt').toString()
+
+const result = sum(
+    input
+        .split('\n')
+        .map(line => line == "" ? undefined : Number(line))
+        .reduce((partial, next) =>
+            next === undefined
+                ? [...partial, []]
+                : [...partial.slice(0, -1), [...partial[partial.length -1], next]]
+        , [[]])
+        .map(sum)
+        .sort()
+        .slice(-3)
+)
+
+console.log(result)
